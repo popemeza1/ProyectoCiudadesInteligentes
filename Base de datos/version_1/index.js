bef8e5c9f8c8522ff07ab62fa421b53faa467ecc
alejohernandez95 <alejohernandez95@hotmail.com>
2018-11-01 01:29:36 -0500

    actualizacion
    actualizando cosas

--- Base de datos/version_1/index.js
+++ Base de datos/version_1/index.js
@@ -10,7 +10,19 @@ app.get('/appi/sensor',(req,res) => {
   res.send(200,{sensores:[]})
 })
 //muestra un sensor en especifico
-app.get('/appi/sensores/sensoresId',(req,res) => {
+app.get('/appi/sensores/:sensoresId',(req,res) => {
 
+})
+//sube datos de un sensor
+app.post('/appi/sensores',(req,res) => {
+  console.log(req.body)
+  res.send(200,{message:'los datos se han recibido'})
+})
+//actualiza los datos del sensor
+app.put('/appi/sensores/:sensoresId',(req,res) => {
+
+})
+app.delete('/appi/sensores/:sensoresId',(req,res) => {
+//borra los datos de un sensor
 })
 app.listen(port,() => {console.log(`API REST corriendo en http://localhost:${port}`)})
